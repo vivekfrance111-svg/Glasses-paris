4841a0feff65c4a17a6b6498aaabf8e7bbb3ce5e
Vivek <vivekfrance111@gmail.com>
2026-03-05 21:23:45 +0100

    ready to deploy

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,8 +6,7 @@ const sendEmail = async (options) => {
 
     // Create a transporter
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
